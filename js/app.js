b921b1e7e8d8ee81df8b36ba2feec4aa26d8e59c
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2017-01-19 22:46:20 +0200

    create project skileton

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 /**
  * Created by Iaroslav Zhbankov on 19.01.2017.
  */
+
+var RecipeBox = React.createClass({
+    render: function(){
+        return(
+            <div className='container'>Container</div>
+        )
+    }
+});
+
+ReactDOM.render(
+    <RecipeBox />,
+    document.getElementById('box')
+);
