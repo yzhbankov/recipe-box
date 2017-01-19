e68acb9e0314c48b61ded07c50ff8f682130009d
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2017-01-19 23:13:36 +0200

    create project skileton

--- js/app.js
+++ js/app.js
@@ -1,16 +1,38 @@
 /**
  * Created by Iaroslav Zhbankov on 19.01.2017.
  */
-
+var recipes = [];
 var RecipeBox = React.createClass({
-    render: function(){
-        return(
-            <div className='container'>Container</div>
+    getInitialState: function () {
+        return {
+            recipes: recipes
+        }
+    },
+    addRecipe: function (e) {
+        e.preventDefault();
+        this.setState({
+            recipes: recipes.concat([{
+                "name": 'Name', "ing": 'Ingridients'
+            }])
+        })
+    },
+    render: function () {
+        return (
+            <div>
+                <div className='container'>Recipe Box</div>
+                {this.props.recipes.map(function (item) {
+                    <div>
+                        <div>{item['name']}</div>
+                        <div>{item['ing']}</div>
+                    </div>
+                })}
+                <button className='btn btn-primary' onClick={this.addRecipe}>Add recipe</button>
+            </div>
         )
     }
 });
 
 ReactDOM.render(
-    <RecipeBox />,
+    <RecipeBox recipes={recipes}/>,
     document.getElementById('box')
 );
